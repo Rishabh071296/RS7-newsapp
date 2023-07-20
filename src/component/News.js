e8fd29dcf886b1e3eac4564aae766cb4ebe0cfcf
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-20 23:38:02 +0530

    Update

--- src/component/News.js
+++ src/component/News.js
@@ -2,71 +2,95 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class News extends Component {
-  articles= [
-    {
-        "source": {
-            "id": "news24",
-            "name": "News24"
-        },
-        "author": "Heinz Schenk",
-        "title": "EXPLAINED | World Rugby goes T20 with bold new sevens series format to draw non-believers",
-        "description": "In what could perhaps be considered rugby's attempt to harness the pulling power of T20 cricket with younger audiences, World Rugby on Tuesday showed its hand in anointing the sevens format as its trump card for increasing the reach of the game.",
-        "url": "https://www.news24.com/sport/rugby/sevens/explained-world-rugby-goes-t20-with-bold-new-sevens-series-format-to-draw-non-believers-20230718",
-        "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/11010/bd91a8cc64834456a9927e08a3c4d263.jpg",
-        "publishedAt": "2023-07-18T17:06:40+00:00",
-        "content": "<ul><li>World Rugby on Tuesday announced a re-branded sevens series named \"SVNS\".</li><li>The aim is to place a premium on match-day experiences in order to reach a younger and more non-traditional r… [+5579 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-]
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-    articles: this.articles,
-    loading: false
-    }
+      articles: [],
+      loading: false,
+      page: 1,
+    };
   }
-  
+  async componentDidMount() {
+    console.log("cdm");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5c38f28dc5a1478b9c5df54dcffe7463&page=1pageSized=20 ";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5c38f28dc5a1478b9c5df54dcffe7463&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handlenextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } 
+    else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5c38f28dc5a1478b9c5df54dcffe7463&page=${
+        this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="cotainer my-3">
-        <h2>RS7 News - Top Headlines</h2>
-       
+        <h1 className="text-center">RS7 News - Top Headlines</h1>
+
         <div className="row">
-        {this.state.articles.map((element)=>{
-          return <div className="col-md-3" key={element.url}>
-            <Newsitem  title={element.title} description={element.description}imageUrl={element.urlToImage} newsUrl= {element.url}/>
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <Newsitem
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
-          
-          </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.pages <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlenextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
