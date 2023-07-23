688322d76a962f1d70d86394c8936554e3fba164
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-23 22:41:57 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router> 
         <NavBar/>
          <Switch>
-          <Route path="/"><News pageSize={8} county = "in" category="General" /></Route>
-          <Route path="/Business"><News pageSize={8} county = "in" category="Business" /></Route>
-          <Route path="/Entertainment"><News pageSize={8} county = "in" category="Entertainment" /></Route>
-          <Route path="/General"><News pageSize={8} county = "in" category="General" /></Route>
-          <Route path="/Health"><News pageSize={8} county = "in" category="Health" /></Route>
-          <Route path="/Science"><News pageSize={8} county = "in" category="Science" /></Route>
-          <Route path="/Sport"><News pageSize={8} county = "in" category="Sport" /></Route>
-          <Route path="/Technology"><News pageSize={8} county = "in" category="Technology" /></Route>
+          <Route path="/"><News key="General" pageSize={8} county = "in" category="General" /></Route>
+          <Route path="/Business"><News key="Business" pageSize={8} county = "in" category="Business" /></Route>
+          <Route path="/Entertainment"><News key="Entertainment" pageSize={8} county = "in" category="Entertainment" /></Route>
+          <Route path="/General"><News key="General" pageSize={8} county = "in" category="General" /></Route>
+          <Route path="/Health"><News key="Health" pageSize={8} county = "in" category="Health" /></Route>
+          <Route path="/Science"><News key="Science" pageSize={8} county = "in" category="Science" /></Route>
+          <Route path="/Sport"><News key="Sport" pageSize={8} county = "in" category="Sport" /></Route>
+          <Route path="/Technology"><News key="Technology" pageSize={8} county = "in" category="Technology" /></Route>
           </Switch>
         </Router>
       </div>
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -8,9 +8,9 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid"> 
-            <link className="navbar-brand" to="/">
-             🆁🆂7 News
-            </link>
+          <Link className="navbar-brand" to="/">
+           🆁🆂7 News
+           </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -39,8 +39,7 @@ export class NavBar extends Component {
                 <li className="nav-item"><Link className="nav-link" to="./Sport">Sport </Link></li>
                 <li className="nav-item"><Link className="nav-link" to="./Technology">Technology </Link></li>
 
-                
-                 </ul>
+               </ul>
             </div>
           </div>
         </nav>
